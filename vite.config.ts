e6f99a2c8a81b7650d89a576c61a3f4f9c586f58
ikenkhj <ikenkhj@github.com>
2023-09-06 14:12:04 +0800

    eslint+prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,33 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+//自动引入组件的插件
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+import path  from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    //自动引入插件的组件配置
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   //开发服务器
   server:{
     // port:3000
+  },
+  resolve:{
+    // 配置路径别名
+    alias:{
+      '@':path.resolve(__dirname,'src')
+    }
   }
 })
